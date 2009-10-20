492ded0785c99768393edc2d2acc77a95047878e
Simon Arlott <simon.arlott.org.uk>
2009-10-20 13:07:51 +0100

    Always require a HELO command
    (As per RFC 2821 4.1.1.1)

--- src/src/smtp_in.c
+++ src/src/smtp_in.c
@@ -3197,11 +3197,12 @@ while (done <= 0)
     smtp_mailcmd_count++;              /* Count for limit and ratelimit */
     was_rej_mail = TRUE;               /* Reset if accepted */
 
-    if (helo_required && !helo_seen)
+    /* RFC 2821 4.1.1.1:
+       "In any event, a client MUST issue HELO or EHLO before starting a mail transaction" */
+    if (/*helo_required &&*/ !helo_seen)
       {
-      smtp_printf("503 HELO or EHLO required\r\n");
-      log_write(0, LOG_MAIN|LOG_REJECT, "rejected MAIL from %s: no "
-        "HELO/EHLO given", host_and_ident(FALSE));
+      done = synprot_error(L_smtp_protocol_error, 503, NULL,
+        US"HELO/EHLO required");
       break;
       }
 
